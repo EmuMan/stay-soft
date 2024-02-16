a7c2c1f1f7f6c94555fe44559bea89c2dc8c83ad
Colin McDonald <1colinmcdonald@gmail.com>
2024-02-15 19:00:57 -0800

    Practice with material ui

---
+++ packages/react-frontend/src/components/MuiButton.js
@@ -0,0 +1,36 @@
+import { Stack, Button } from "@mui/material";
+let MuiButton = () => {
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+    </Stack>
+  );
+};
+
+export { MuiButton };
