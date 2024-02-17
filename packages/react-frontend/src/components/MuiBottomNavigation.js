aa19ed310cc315e210490ec9dbe30cb6a4ff4a85
Colin McDonald <1colinmcdonald@gmail.com>
2024-02-17 15:52:17 -0800

    Add changing result after clicking tabs

--- packages/react-frontend/src/components/MuiBottomNavigation.js
+++ packages/react-frontend/src/components/MuiBottomNavigation.js
@@ -1,4 +1,9 @@
-import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Typography,
+} from "@mui/material";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import HomeIcon from "@mui/icons-material/Home";
 import AddBoxIcon from "@mui/icons-material/AddBox";
@@ -6,18 +11,34 @@ import { useState } from "react";
 
 const MuiBottomNavigation = () => {
   const [value, setValue] = useState(0);
+  const compArray = [];
+  const createNavComponent = (text) => {
+    return (
+      <Box display="flex" justifyContent="center" key={text}>
+        <Typography variant="H5">{text}</Typography>
+      </Box>
+    );
+  };
+  compArray.push(createNavComponent("Home"));
+  compArray.push(createNavComponent("Create"));
+  compArray.push(createNavComponent("Profile"));
+  console.log(value);
   return (
-    <BottomNavigation
-      sx={{ width: "100%", position: "absolute", bottom: 0 }}
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-    >
-      <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-      <BottomNavigationAction label="Create" icon={<AddBoxIcon />} />
-      <BottomNavigationAction label="Profile" icon={<AccountCircleIcon />} />
-    </BottomNavigation>
+    <div>
+      {compArray[value]}
+      <BottomNavigation
+        sx={{ width: "100%", position: "absolute", bottom: 0 }}
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+      >
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Create" icon={<AddBoxIcon />} />
+        <BottomNavigationAction label="Profile" icon={<AccountCircleIcon />} />
+      </BottomNavigation>
+    </div>
   );
 };
 
