517d80320cd9700eabde004f51fa89e8222cc3c6
Colin McDonald <1colinmcdonald@gmail.com>
2024-02-27 13:06:26 -0800

    Get rid of visibility stuff

--- packages/react-frontend/src/components/Bet.js
+++ packages/react-frontend/src/components/Bet.js
@@ -19,61 +19,42 @@ import { useState } from "react";
 import BetAmount from "./BetAmount.js";
 
 function Bet(prop) {
-  const [formVisible, setFormVisible] = useState(false);
-  const [betVisible, setbetVisible] = useState(true);
-  let visibility = "hidden";
   return (
     <Box width="95%">
-      {betVisible && (
-        <Card
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            marginTop: "0",
-          }}
-        >
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {prop.question.description}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Box>
-              <FormControl>
-                <RadioGroup name="decision" aria-labelledby="decision-label">
-                  <FormControlLabel
-                    control={<Radio />}
-                    label="Yes"
-                    value="Yes"
-                    onClick={() => setFormVisible(true)}
-                  />
-                  <FormControlLabel
-                    control={<Radio />}
-                    label="No"
-                    value="No"
-                    onClick={() => setFormVisible(true)}
-                  />
-                </RadioGroup>
-              </FormControl>
-            </Box>
-            <Stack>
-              {formVisible && (
-                <TextField
-                  type="number"
-                  label="Amount"
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position="start">$ </InputAdornment>
-                    ),
-                  }}
-                />
-              )}
-              <Button onClick={() => setbetVisible(false)}>Confirm</Button>
-            </Stack>
-          </CardActions>
-        </Card>
-      )}
+      <Card
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {prop.question.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Box>
+            <FormControl>
+              <RadioGroup name="decision" aria-labelledby="decision-label">
+                <FormControlLabel control={<Radio />} label="Yes" value="Yes" />
+                <FormControlLabel control={<Radio />} label="No" value="No" />
+              </RadioGroup>
+            </FormControl>
+          </Box>
+          <Stack>
+            <TextField
+              label="Amount"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$ </InputAdornment>
+                ),
+              }}
+            />
+            <Button>Confirm</Button>
+          </Stack>
+        </CardActions>
+      </Card>
     </Box>
   );
 }
