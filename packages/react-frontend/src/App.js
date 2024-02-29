fea028a35fabe02b8928f261a23cb325c378f48b
Colin McDonald <1colinmcdonald@gmail.com>
2024-02-29 15:52:46 -0800

    Make the primary color green and the secondary color gold

--- packages/react-frontend/src/App.js
+++ packages/react-frontend/src/App.js
@@ -3,7 +3,18 @@ import { useState } from "react";
 import Navigation from "./Navigation";
 import Login from "./pages/Login.js";
 import Signup from "./pages/Signup.js";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#154734",
+    },
+    secondary: {
+      main: "#BD8B13",
+    },
+  },
+});
 
 function App() {
   const [loggedIn, setLogin] = useState(false);
@@ -11,15 +22,23 @@ function App() {
   function onLogin() {
     setLogin(true);
   }
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<Login loggedIn={loggedIn} onLogin={onLogin} />} />
-        <Route path="/navigation" element={<Navigation loggedIn={loggedIn} />} />
-        <Route path="/signup" element={<Signup onLogin={onLogin} />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            exact
+            element={<Login loggedIn={loggedIn} onLogin={onLogin} />}
+          />
+          <Route
+            path="/navigation"
+            element={<Navigation loggedIn={loggedIn} />}
+          />
+          <Route path="/signup" element={<Signup onLogin={onLogin} />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
