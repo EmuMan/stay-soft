5e8998a374f3f1a9d9834f270228e9c4707844bf
Colin McDonald <1colinmcdonald@gmail.com>
2024-02-29 15:58:46 -0800

    Change single quotes to double quotes

--- packages/react-frontend/src/pages/Login.js
+++ packages/react-frontend/src/pages/Login.js
@@ -1,38 +1,38 @@
 import { Button, TextField, Typography } from "@mui/material";
 import "./Login.css";
 import { useNavigate } from "react-router-dom";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function Login(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     if (props.loggedIn) {
-      navigate('/navigation');
+      navigate("/navigation");
     }
   }, [props.loggedIn, navigate]);
 
   async function loginUser() {
     try {
-      const response = await fetch('http://localhost:8000/users/login', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8000/users/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ email, password }),
       });
       const data = await response.json();
       if (response.ok) {
         props.onLogin();
-        navigate('/navigation');
+        navigate("/navigation");
       } else {
         alert(data.message);
       }
     } catch (error) {
-      console.error('Login failed:', error);
-      alert('Login request failed');
+      console.error("Login failed:", error);
+      alert("Login request failed");
     }
   }
 
@@ -41,8 +41,19 @@ function Login(props) {
       <Typography variant="h5" component="div" align="center">
         PolyPicks • Login
       </Typography>
-      <TextField label="Email" className="text-input" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <TextField label="Password" type="password" className="text-input" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <TextField
+        label="Email"
+        className="text-input"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        className="text-input"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <Button className="elevated-button" onClick={loginUser}>
         <Typography variant="h6" component="div">
           Login
@@ -51,7 +62,7 @@ function Login(props) {
       <Typography variant="h6" component="div" align="center">
         Don't have an account?
       </Typography>
-      <Button className="button" onClick={() => navigate('/signup')}>
+      <Button className="button" onClick={() => navigate("/signup")}>
         <Typography variant="h6" component="div">
           Signup
         </Typography>
--- packages/react-frontend/src/pages/Signup.js
+++ packages/react-frontend/src/pages/Signup.js
@@ -28,25 +28,25 @@ function Signup(props) {
     }
 
     try {
-      const response = await fetch('http://localhost:8000/users/signup', {
-        method: 'POST',
+      const response = await fetch("http://localhost:8000/users/signup", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           email,
           username,
           password,
           firstName,
-          lastName
+          lastName,
         }),
       });
 
       if (response.ok) {
         const data = await response.json();
-        console.log('Signup successful', data);
+        console.log("Signup successful", data);
         props.onLogin();
-        navigate('/navigation');
+        navigate("/navigation");
       } else {
         const error = await response.text();
         throw new Error(error);
@@ -61,12 +61,44 @@ function Signup(props) {
       <Typography variant="h5" component="div" align="center">
         Create an account
       </Typography>
-      <TextField label="Email" value={email} onChange={e => setEmail(e.target.value)} className="text-input" />
-      <TextField label="Username" value={username} onChange={e => setUsername(e.target.value)} className="text-input" />
-      <TextField label="First Name" value={firstName} onChange={e => setFirstName(e.target.value)} className="text-input" />
-      <TextField label="Last Name" value={lastName} onChange={e => setLastName(e.target.value)} className="text-input" />
-      <TextField label="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} className="text-input" />
-      <TextField label="Repeat password" type="password" value={repeatPassword} onChange={e => setRepeatPassword(e.target.value)} className="text-input" />
+      <TextField
+        label="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="text-input"
+      />
+      <TextField
+        label="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        className="text-input"
+      />
+      <TextField
+        label="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        className="text-input"
+      />
+      <TextField
+        label="Last Name"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+        className="text-input"
+      />
+      <TextField
+        label="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="text-input"
+      />
+      <TextField
+        label="Repeat password"
+        type="password"
+        value={repeatPassword}
+        onChange={(e) => setRepeatPassword(e.target.value)}
+        className="text-input"
+      />
       <Button className="elevated-button" onClick={signupUser}>
         <Typography variant="h6" component="div">
           Signup
@@ -75,7 +107,7 @@ function Signup(props) {
       <Typography variant="h6" component="div" align="center">
         Already have an account?
       </Typography>
-      <Button className="button" onClick={() => navigate('/')}>
+      <Button className="button" onClick={() => navigate("/")}>
         <Typography variant="h6" component="div">
           Login
         </Typography>
