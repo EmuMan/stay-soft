80bf0c8191a45db39538e81ca459e2f8aaead711
Colin McDonald <1colinmcdonald@gmail.com>
2024-03-04 12:16:51 -0800

    Make the

--- packages/react-frontend/src/components/creatorPrompt.js
+++ packages/react-frontend/src/components/creatorPrompt.js
@@ -1,6 +1,22 @@
-import { Box, Typography, Button, Stack } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  CardMedia,
+  TextField,
+  InputAdornment,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+  Stack,
+} from "@mui/material";
 
-const creatorPrompt = (prop) => {
+const CreatorPrompt = (prop) => {
   return (
     <Box width="95%">
       <Card
@@ -12,7 +28,7 @@ const creatorPrompt = (prop) => {
       >
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {prop.question.question}
+            {prop.question}
           </Typography>
         </CardContent>
         <CardActions>
@@ -25,14 +41,6 @@ const creatorPrompt = (prop) => {
             </FormControl>
           </Box>
           <Stack>
-            <TextField
-              label="Amount"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">$ </InputAdornment>
-                ),
-              }}
-            />
             <Button>Confirm</Button>
           </Stack>
         </CardActions>
@@ -40,57 +48,5 @@ const creatorPrompt = (prop) => {
     </Box>
   );
 };
-const creatorPrompt = (props) => {
-  const betContainerStyle = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
-    borderTop: "1px solid black",
-    borderBottom: "1px solid black",
-    padding: "1rem 0",
-    margin: "1rem",
-    width: "100%",
-    gap: "1rem",
-    backgroundColor: "white",
-  };
 
-  return (
-    <Box style={betContainerStyle}>
-      <Stack spacing={2} direction="row">
-        <Typography variant="h6" component="div">
-          Date Opened: {props.dateOpen}
-        </Typography>
-        <Typography variant="h6" component="div">
-          Date Closed: {props.dateClose}
-        </Typography>
-      </Stack>
-      <Typography variant="h6" component="div">
-        {props.description}
-      </Typography>
-      <Typography variant="h6" component="div">
-        Category: {props.category}
-      </Typography>
-      <Typography variant="h6" component="div">
-        Creator: {props.creator}
-      </Typography>
-      <Typography variant="h6" component="div">
-        yes: {props.yesCount}
-      </Typography>
-      <Typography variant="h6" component="div">
-        no: {props.noCount}
-      </Typography>
-      <Stack spacing={2} direction="row">
-        <Button variant="contained" style={{ textTransform: "none" }}>
-          Yes
-        </Button>
-        <Button variant="contained" style={{ textTransform: "none" }}>
-          No
-        </Button>
-      </Stack>
-      <Typography variant="h6" color="#0000FF" component="div">
-        Comments
-      </Typography>
-    </Box>
-  );
-};
+export default CreatorPrompt;
