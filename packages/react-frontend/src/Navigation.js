48bfd19a6f327a468af2f64495deb53b4f05cf7b
Colin McDonald <1colinmcdonald@gmail.com>
2024-03-04 12:18:00 -0800

    .

--- packages/react-frontend/src/Navigation.js
+++ packages/react-frontend/src/Navigation.js
@@ -8,13 +8,37 @@ import CreatePage from "./pages/CreatePage.js";
 import ProfilePage from "./pages/ProfilePage.js";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 function Navigation(props) {
   const [pageIndex, setPageIndex] = useState(0);
-  const [bets, setBets] = useState([]);
-  const compArray = [];
   const navigate = useNavigate();
-
+  const [profile, setProfile] = useState({
+    username: "",
+    points: 0,
+    betsWon: 0,
+    betsLost: 0,
+    respondents: 0,
+    theme: 0,
+    bets: [],
+  });
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      const userId = decodedToken.id;
+      fetch(`http://localhost:8000/users/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setProfile(data);
+        })
+        .catch((error) => console.error("Error:", error));
+    }
+  }, []);
   useEffect(() => {
     if (!props.loggedIn) {
       navigate("/");
@@ -22,20 +46,20 @@ function Navigation(props) {
   }, [navigate, props.loggedIn]);
 
   const handleSignOut = () => {
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     props.setLoggedIn(false);
     navigate("/");
   };
 
   return (
     <div className="navigation">
-      <TopBar points={props.profile.points} onSignOut={handleSignOut} />
+      <TopBar points={profile.points} onSignOut={handleSignOut} />
       <Stack padding="20px" marginBottom="60px">
         {
           [
-            <HomePage profile={props.profile} />,
-            <CreatePage profile={props.profile} />,
-            <ProfilePage profile={props.profile} />,
+            <HomePage profile={profile} />,
+            <CreatePage profile={profile} />,
+            <ProfilePage profile={profile} />,
           ][pageIndex]
         }
       </Stack>
--- packages/react-frontend/src/pages/ProfilePage.js
+++ packages/react-frontend/src/pages/ProfilePage.js
@@ -1,8 +1,24 @@
 import { useState, useEffect } from "react";
 import { Box, Typography, Stack } from "@mui/material";
 import { jwtDecode } from "jwt-decode";
+import CreatorPromptsFeed from "/Users/colinmcdonald/Library/CloudStorage/GoogleDrive-1colinmcdonald@gmail.com/My Drive/college/courses/CSC/CSC 307/PolyPicks-repo/packages/react-frontend/src/components/CreatorPromptsFeeds.js";
 
 const ProfilePage = (props) => {
+  const [prompts, setPrompts] = useState([]);
+  function fetchPrompts() {
+    const promise = fetch(
+      "http://localhost:8000/prompts?user=" + props.profile["_id"]
+    );
+    return promise;
+  }
+  useEffect(() => {
+    fetchPrompts()
+      .then((res) => res.json())
+      .then((json) => setPrompts(json))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <Stack
       style={{
@@ -20,6 +36,7 @@ const ProfilePage = (props) => {
       <Typography variant="h6">
         Respondents: {props.profile.respondents}
       </Typography>
+      <CreatorPromptsFeed prompts={prompts} />
     </Stack>
   );
 };
