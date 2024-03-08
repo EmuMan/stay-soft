87850c3fcbee28d0b14e64b7e3ddc28f2601fa08
Colin McDonald <1colinmcdonald@gmail.com>
2024-03-08 14:34:35 -0800

    Get rid of warnings

--- packages/react-frontend/src/components/MyBetsFeed.js
+++ packages/react-frontend/src/components/MyBetsFeed.js
@@ -1,25 +1,8 @@
 import React from "react";
-import CreatorPrompt from "./CreatorPrompt.js";
 import MyBet from "./MyBet.js";
-import { useState, useEffect } from "react";
 
 function MyBetsFeed(props) {
-  const [bets, setBets] = useState(props.bets);
-  let question = null;
   const myBets = props.bets.map((bet, index) => {
-    const fetchPrompt = async () => {
-      const response = await fetch(
-        `${process.env.REACT_APP_API_ENDPOINT}/prompts/${bet.promptId}`
-      );
-      const promptData = await response.json();
-      question = promptData.question;
-      setBets((prevBets) =>
-        prevBets.map((b) =>
-          b._id === bet._id ? { ...b, prompt: promptData } : b
-        )
-      );
-    };
-    fetchPrompt();
     return (
       <MyBet
         key={index}
