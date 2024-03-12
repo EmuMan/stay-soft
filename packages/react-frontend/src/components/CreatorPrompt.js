f1ff5f3a1d4182e0b11697e4c413690392d1d389
Colin McDonald <1colinmcdonald@gmail.com>
2024-03-12 13:32:45 -0700

    Base my changes off of 706f6b9

--- packages/react-frontend/src/components/CreatorPrompt.js
+++ packages/react-frontend/src/components/CreatorPrompt.js
@@ -11,8 +11,44 @@ import {
   Radio,
   Stack,
 } from "@mui/material";
+import { useState } from "react";
 
 const CreatorPrompt = (prop) => {
+  const [result, setResult] = useState("");
+  const [bets, setBets] = useState([]);
+  const [betters, setBetters] = useState([]);
+
+  const handleResolution = () => {
+    if (result !== "") {
+      if (result === "Yes") {
+        result = true;
+      } else if (result === "No") {
+        result = false;
+      }
+      const fetchBets = () => {
+        return fetch(
+          `${process.env.REACT_APP_API_ENDPOINT}/bets?promptId=${prop.prompt._id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+      };
+      fetchBets()
+        .then((res) => res.json())
+        .then((json) => setBets(json))
+        .catch((error) => {
+          console.log(error);
+        });
+      const winningBets = bets.filter((bet) => bet.decision === result);
+      // use patch to update each user's points
+    }
+  };
+  const handleChange = (event) => {
+    setResult(event.target.value);
+  };
+
   return (
     <Box width="95%">
       <Card
